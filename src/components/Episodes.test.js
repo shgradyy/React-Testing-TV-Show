2475127b780e312269ce4d974ac4630af7a281f5
shgradyy <shantaygrady@gmail.com>
2020-04-22 23:27:45 -0400

    Mock Data setup

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -4,8 +4,11 @@ import { render } from '@testing-library/react';
 import Episodes from './Episodes';
 
 test("Episodes renders with selected season", ()=>{
-    render(<Episodes episodes="" />);
-    // setup
-    // execute
-    // assert
+    render(<Episodes episodes={[]} />);
+  
+});
+
+test("Renders Episodes with images", ()=>{
+    const mockData = [{}];
+    render(<Episodes )
 })
