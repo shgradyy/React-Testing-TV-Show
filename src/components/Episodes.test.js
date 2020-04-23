31e7cca1237ec3c71d0027f6e55f013f28c6d477
shgradyy <shantaygrady@gmail.com>
2020-04-22 23:12:31 -0400

    test setup

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Episodes from './Episodes';
+
+test("Episodes renders with selected season", ()=>{
+    render(<Episodes episodes="" />);
+    // setup
+    // execute
+    // assert
+})
